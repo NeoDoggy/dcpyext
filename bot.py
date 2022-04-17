e98e457502340619cd7f7e40bc3552b599751f2c
NeoDoggy <neodog0616@gmail.com>
2022-04-17 15:42:42 +0800

    remove slash cog

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import os
-#from posix import uname_result
 import discord 
 from discord.ext import commands
 from dotenv import load_dotenv
 from discord_ui import *
 from discord_slash import *
-#from datetime import datetime as dt
+
 
 from cogs import music 
 from cogs import main
@@ -14,7 +13,6 @@ from cogs import twt
 from cogs import help
 from cogs import covid
 from cogs import sauce
-#from cogs import slashC
 
 load_dotenv()
 token = os.getenv('bot_token')
@@ -22,7 +20,7 @@ prefix = os.getenv('prefix')
 
 bot=commands.Bot(command_prefix=prefix)
 ui = UI(bot,override_dpy=True)
-#slash = SlashCommand(bot)
+
 
 bot.remove_command('help')
 
@@ -33,7 +31,7 @@ bot.add_cog(twt.twt_cog(bot))
 bot.add_cog(help.help_cog(bot))
 bot.add_cog(covid.covid_cog(bot))
 bot.add_cog(sauce.sauce_cog(bot))
-#bot.add_cog(slashC.slash_cog(bot))
+
 
 
 bot.run(token)
